c3ff6fdea559ec39a36435d2a4cc994a998b18e8
AkmalSeptiyana <takaakmal10969@gmail.com>
2022-12-10 14:24:45 +0700

    fix error

--- components/layout/global-header.js
+++ components/layout/global-header.js
@@ -56,7 +56,7 @@ export function GlobalHeader() {
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 30 24"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               stroke="currentColor"
               className="w-[30px] h-6"
             >
